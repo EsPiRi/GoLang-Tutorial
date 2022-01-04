80318fc5f9fd7523af95ffb2a4b2d67026c24e0c
Sami Sezgin <sezginsami@gmail.com>
2022-01-04 15:55:40 +0300

    added subtracting remaining tickets

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ func main() {
 
 	var conferenceName string = "Go Conference"
 
-	const conferenceTickets int = 50
-	var remainingTickets int = conferenceTickets
+	const conferenceTickets uint = 50
+	var remainingTickets uint = conferenceTickets
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	//fmt.Println("Welcome to", conferenceName, "booking application")
@@ -22,7 +22,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -36,6 +36,8 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	remainingTickets -= userTickets
 
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
